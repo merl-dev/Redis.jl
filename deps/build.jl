ada9566c7d7b0ef89bc2a0631c512350313950a7
merl-dev <john@fpsignals.com>
2016-07-11 12:20:34 +0300

    travis fix

--- deps/build.jl
+++ deps/build.jl
@@ -5,15 +5,6 @@ using Compat
 
 libhiredis = library_dependency("libhiredis")
 
-# package managers
 provides(AptGet, Dict("libhiredis-dev"=>libhiredis))
-
-@osx_only begin
-    if Pkg.installed("Homebrew") === nothing
-        error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
-    end
-    using Homebrew
-    provides(Homebrew.HB, "hiredis", libgsl, os = :Darwin)
-end
 
 @BinDeps.install Dict(:libhiredis => :libhiredis)
